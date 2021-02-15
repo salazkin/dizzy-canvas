4fdd5dfeb0e51e77323fad9c5734cf4cc2a875d3
AlexeySalazkin <salazkin@gmail.com>
2021-02-15 12:51:34 +0200

    update version, update rollup config, fix npmignore

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
-    input: 'src/Lib.js',
-    output: {
+    input: 'src/index.js',
+    output: [{
         file: 'dist/dizzy-canvas.js',
-        format: 'es'
-    }
+        format: 'es',
+        sourcemap: true
+    }, {
+        file: 'dist/dizzy-canvas.min.js',
+        format: 'es',
+        plugins: [terser()],
+        sourcemap: true
+    }]
 };
